031003b618857c383bf8407afeea651c9670ab72
Marcus Mellor <infinitymdm@gmail.com>
2022-09-26 21:27:57 -0500

    Add prototype code for testing motors

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,12 +1,25 @@
+#define MIC_IN PIN_A6   // Digital input from microphone module
+#define L_MOTOR PIN_A10 // Digital motor control output
+#define R_MOTOR PIN_A9  // Digital motor control output
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+  // Set up pins
+  pinMode(MIC_IN, INPUT);
+  pinMode(L_MOTOR, OUTPUT);
+  pinMode(R_MOTOR, OUTPUT);
+
+  // Initialize with motors off
+  digitalWrite(L_MOTOR, LOW);
+  digitalWrite(R_MOTOR, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(300);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(700);
+  digitalWrite(L_MOTOR, HIGH);
+  delay(250);
+  digitalWrite(R_MOTOR, HIGH);
+  delay(250);
+  digitalWrite(R_MOTOR, LOW);
+  delay(250);
+  digitalWrite(L_MOTOR, LOW);
+  delay(250);
 }
