c7a3bb25e0a1fa3ee38f23d0de46d82fa7fb4984
Marcus Mellor <infinitymdm@gmail.com>
2022-09-29 18:46:37 -0500

    Add readme and license

---
+++ readme.md
@@ -0,0 +1,6 @@
+Once complete, this repository will contain all resources necessary to build and 
+program a basic arduino robot that steers based on audio frequency, plus a small
+theremin-like controller. 
+
+The purpose of this project is to introduce future engineers to a variety of electrical,
+mechanical, and computer engineering concepts. 
--- robot/robot.ino
+++ robot/robot.ino
@@ -1,62 +1,62 @@
-#define MIC_IN PIN_A3   // Analog input from microphone module
-#define MIC_ACTIVE PIN_A6   // Digital input from microphone module
-#define L_MOTOR PIN_A10 // Digital motor control output
-#define R_MOTOR PIN_A9  // Digital motor control output
+#define MIC_IN 3        // Analog input from microphone module
+#define MIC_ACTIVITY 6  // Digital input from microphone module
+#define L_MOTOR 10      // Digital motor control output
+#define R_MOTOR 9       // Digital motor control output
 
 void setup() {
   // Set up pins
   pinMode(MIC_IN, INPUT);
+  pinMode(MIC_ACTIVITY, INPUT);
   pinMode(L_MOTOR, OUTPUT);
   pinMode(R_MOTOR, OUTPUT);
 
   // Initialize with motors off
-  digitalWrite(L_MOTOR, HIGH);
-  digitalWrite(R_MOTOR, HIGH);
+  stop();
 }
 
 void loop() {
-  digitalWrite(L_MOTOR, LOW);
-  delay(2000);
-  digitalWrite(L_MOTOR, HIGH);
+  turnRight();
+  delay(1000);
+  stop();
   delay(500);
-  digitalWrite(R_MOTOR, LOW);
-  delay(2000);
-  digitalWrite(R_MOTOR, HIGH);
-  delay(500);
-
-//  if(MIC_IN >= X)
-//    turnLeft();
-
-//  else if (MIC_IN <= Y)
-//    turnRight();
-  
-//  else if (MIC_IN == Z)
-//    stopRobot();
-
-//  else
-//    goForward();
+  turnLeft();
+  delay(1000);
+  stop();
+  delay(100);
 }
 
 // Turn the left motor and the right motor equally
-void goForward(){
-  digitalWrite(L_MOTOR, HIGH);
-  digitalWrite(R_MOTOR, HIGH);
+void go() {
+  setLeftMotor(HIGH);
+  setRightMotor(HIGH);
 }
 
 // Stop both motors
-void stopRobot(){
-  digitalWrite(L_MOTOR, LOW);
-  digitalWrite(R_MOTOR, LOW);
+void stop() {
+  setLeftMotor(LOW);
+  setRightMotor(LOW);
 }
 
 // Turn the left motor less than the right motor to turn left
-void turnLeft(){
-  digitalWrite(L_MOTOR, LOW);
-  digitalWrite(R_MOTOR, HIGH);
+void turnLeft() {
+  setLeftMotor(LOW);
+  setRightMotor(HIGH);
 }
 
 // Turn the left motor more than the right motor to turn right
-void turnRight(){
-  digitalWrite(L_MOTOR, HIGH);
-  digitalWrite(R_MOTOR, LOW);  
+void turnRight() {
+  setRightMotor(LOW);
+  setLeftMotor(HIGH);
+}
+
+// Abstract left motor
+// TODO: add an LED
+void setLeftMotor(bool mode) {
+  digitalWrite(L_MOTOR, !mode);
+}
+
+// Abstract right motor
+// TODO: add an LED
+void setRightMotor(bool mode) {
+  digitalWrite(R_MOTOR, !mode);
 }
