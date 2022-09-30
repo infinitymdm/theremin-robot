c56dd35301e250367ca02b67e837487939445dd6
Marcus Mellor <infinitymdm@gmail.com>
2022-09-30 15:54:06 -0500

    Add audio analysis code

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,28 +1,64 @@
+#include "arduinoFFT.h"
+
+// Pin definitions
+#define MIC_ACTIVITY 2  // Digital input from microphone module
 #define MIC_IN 3        // Analog input from microphone module
-#define MIC_ACTIVITY 6  // Digital input from microphone module
 #define L_MOTOR 10      // Digital motor control output
 #define R_MOTOR 9       // Digital motor control output
+
+// FFT data
+#define SAMPLES 256
+#define SAMPLE_FREQ 12000
+arduinoFFT fft = arduinoFFT();
+unsigned int samplePeriod;
+unsigned long sampleStartTime;
+double vReal[SAMPLES];
+double vImag[SAMPLES];
 
 void setup() {
   // Set up pins
   pinMode(MIC_IN, INPUT);
-  pinMode(MIC_ACTIVITY, INPUT);
   pinMode(L_MOTOR, OUTPUT);
   pinMode(R_MOTOR, OUTPUT);
 
   // Initialize with motors off
   stop();
+
+  // Open Serial port for monitoring
+  Serial.begin(115200);
+
+  // Calculate delay between samples
+  samplePeriod = round(1000000.0/SAMPLE_FREQ);
 }
 
+double peak;
 void loop() {
-  turnRight();
-  delay(1000);
-  stop();
-  delay(500);
-  turnLeft();
-  delay(1000);
-  stop();
-  delay(100);
+  peak = getPeakFrequency();
+  Serial.println(peak);
+
+  // Decide which direction to turn based on peak location
+  // TODO
+}
+
+double getPeakFrequency() {
+  // Collect samples
+  for (int i = 0; i < SAMPLES; i++) {
+    sampleStartTime = micros();
+
+    vReal[i] = analogRead(MIC_IN);
+    vImag[i] = 0;
+
+    // Wait until one sample period has passed
+    while (micros() < (sampleStartTime + samplePeriod));
+  }
+  
+  // Perform FFT on samples
+  fft.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+  fft.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+  fft.ComplexToMagnitude(vReal, vImag, SAMPLES);
+
+  // Identify peak frequency
+  return fft.MajorPeak(vReal, SAMPLES, SAMPLE_FREQ);
 }
 
 // Turn the left motor and the right motor equally
